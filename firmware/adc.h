f4cd878097d441a03627bf9940491c5f5c777fa2
Nima Alamatsaz <nnalamat@gmail.com>
2014-05-17 15:10:51 +0430

    Modified copyright note

--- firmware/adc.h
+++ firmware/adc.h
@@ -6,10 +6,9 @@
  * Email: nnalamat@gmail.com                                             *
  * Web:   http://github.com/nalamat/avroscope                            *
  *                                                                       *
- * AVROscope is free software: you can redistribute it and/or modify     *
- * it under the terms of the GNU General Public License as published by  *
- * the Free Software Foundation, either version 3 of the License, or     *
- * any later version.                                                    *
+ * AVROscope is free software: you can redistribute the software         *
+ * and/or modify it under the terms of the GNU General Public License    *
+ * version 3 as published by the Free Software Foundation.               *
  *                                                                       *
  * AVROscope is distributed in the hope that it will be useful,          *
  * but WITHOUT ANY WARRANTY; without even the implied warranty of        *
